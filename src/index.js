09eb97a2d2a4d4a9053f9fc770a006577be9fba1
Saad ishfaq <sishfaq984@gmail.com>
2024-10-16 16:45:09 +0500

    replaced common js with modular one

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5001;
 
+const jsonObj = {
+  name: "John",
+  age: 16,
+};
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(jsonObj);
 });
 
 app.listen(port, () => {
